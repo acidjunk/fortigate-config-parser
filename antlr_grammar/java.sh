cfb20a4856dba6da68569ce2ad701ece49ab1737
Rene Dohmen <acidjunk@iMac-van-Rene.domain_not_set.invalid>
2016-10-15 19:19:21 +0200

    Script to update stuff

---
+++ antlr_grammar/java.sh
@@ -0,0 +1,9 @@
+#java.sh
+echo "Warning: this will only create lexer and parser in this folder; suitable for experimenting..."
+echo "Use the java.sh from the parent folder to create a distributable parser"
+shopt -s expand_aliases
+source ~/.bash_profile
+antlr4 FortiGate.g4
+javac *.java
+sleep 3
+grun FortiGate file -gui ../conf_examples/production/small.conf
