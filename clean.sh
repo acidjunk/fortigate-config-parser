2f6bff56911caf2e387717c8ce12a251da4ddd93
Rene Dohmen <acidjunk@iMac-van-Rene.domain_not_set.invalid>
2016-10-15 15:48:35 +0200

    Removed makefile and created some utils

---
+++ clean.sh
@@ -0,0 +1,6 @@
+rm -f antlr_grammar/*.py
+rm -f antlr_grammar/*.pyc
+rm -f antlr_grammar/*.lexer
+rm -f antlr_grammar/*.tokens
+rm -f antlr_grammar/*.java
+rm -f antlr_grammar/*.class
---
+++ python2.sh
@@ -0,0 +1,5 @@
+# build python stuff
+shopt -s expand_aliases
+source ~/.bash_profile
+cp antlr_grammar/FortiGate.g4 parsers/python2_parser/
+cd parsers/python2_parser
