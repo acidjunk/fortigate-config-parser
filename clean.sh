7ebc4b41597a9756cff8f5bb8193d783a2a92356
acidjunk <rene.dohmen1@kpn.com>
2016-11-19 14:07:03 +0100

    Cleaned up

--- clean.sh
+++
@@ -1,6 +0,0 @@
-rm -f antlr_grammar/*.py
-rm -f antlr_grammar/*.pyc
-rm -f antlr_grammar/*.lexer
-rm -f antlr_grammar/*.tokens
-rm -f antlr_grammar/*.java
-rm -f antlr_grammar/*.class
--- python2.sh
+++
@@ -1,5 +0,0 @@
-# build python stuff
-shopt -s expand_aliases
-source ~/.bash_profile
-cp antlr_grammar/FortiGate.g4 parsers/python2_parser/
-cd parsers/python2_parser
